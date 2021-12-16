6a16e9ad57e7a5daadd2516291d56244c24cf1a7
Brice Laroche <laroche.brice@gmail.com>
2021-12-16 17:38:31 +0100

    cart.js: var data : localStorage => array

--- P5-Dev-Web-Kanap/front/js/cart.js
+++ P5-Dev-Web-Kanap/front/js/cart.js
@@ -2,4 +2,22 @@
 *       Récupération du panier/array via Local Storage 
 */
 
-let arrayCart = []
+
+
+let getDataFromLocalStorage = (data) => {
+    let arrayCart = []
+
+    if (localStorage.getItem("kanap") == null || localStorage.getItem("kanap") == "undefined") {
+        alert("Le panier est vide")
+    } else {
+        if (localStorage.getItem("kanap")) {
+            arrayCart = JSON.parse(localStorage.getItem("kanap"))
+            console.log(localStorage)
+            console.log(arrayCart)
+        }
+    }
+
+}
+
+
+
